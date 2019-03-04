840286aee753aa39371d7e3b0ad6d981c29771d8
Cuza <baopham92@yahoo.com>
2019-03-04 12:09:50 -0500

    Query resolver for users and self.

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,36 @@
-const Query = {
+import getUserId from '../utils/getUserId'
 
+const Query = {
+    users(parent, args, {
+        prisma
+    }, info) {
+        const opArgs = {}
+
+        if (args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                }]
+            }
+        }
+
+        return prisma.query.users(opArgs, info)
+    },
+    me(parent, args, {
+        prisma,
+        request
+    }, info) {
+        const userId = getUserId(request)
+
+        return prisma.query.user({
+            where: {
+                id: userId
+            }
+        })
+    }
 }
 
-export { Query as default }
+export {
+    Query as
+    default
+}
