1e3346c0027f7b48e790c49f5b41abf2ea4673e7
Cuza <baopham92@yahoo.com>
2019-03-04 19:20:18 -0500

    Utility User resolvers, datamodel additions & typos

--- server/src/resolvers/User.js
+++ server/src/resolvers/User.js
@@ -1,5 +1,23 @@
-const User = {
+import getUserId from '../utils/getUserId'
 
+const User = {
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, {
+            request
+        }, info) {
+            const userId = getUserId(request, false)
+
+            if (userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
+        }
+    }
 }
 
-export { User as default }
+export {
+    User as
+    default
+}
