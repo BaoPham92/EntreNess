3dec63018dd10aab104404f9626ae15b0b16d1d3
Cuza <baopham92@yahoo.com>
2019-03-06 17:21:38 -0500

    Adjusted listners & Token expiration

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill/noConflict'
 import server from './server'
+require('events').EventEmitter.defaultMaxListeners = 100
 
 server.start({ port: process.env.PORT || 4000 }, () => {
     console.log(`Server is running!`)
