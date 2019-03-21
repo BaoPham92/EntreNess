3f603cb8ddf1850aa79c5318574b09c71b071cad
Cuza <baopham92@yahoo.com>
2019-03-21 16:33:02 -0400

    css reset, fav-icon, optional css/scss & typos

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
         }, {
-            test: /\.scss$/,
-            user: [
+            test: /\.s?css$/,
+            use: [
                 'style-loader',
                 'css-loader',
                 'sass-loader'
