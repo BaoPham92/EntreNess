07195820dccb1fba26158c466097d32222294769
Cuza <baopham92@yahoo.com>
2019-05-13 22:55:50 -0400

    NavPanel & UserCP pseudo.

---
+++ client/src/components/view/Header/utils/NavPanel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NavPanel = (props) => {
+
+    return (
+
+    // Dropdown list for navigation content.
+        // Show list of navigational content for Anon & Registered users.
+    )
+}
---
+++ client/src/components/view/Header/utils/UserCP.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const UserCP = (props) => {
+
+    return (
+
+    // Show list of options for Anon & Registered users.
+        // Show list of options for Anon users.
+        // 1. Option sign in
+        // 2. Option Create account
+
+        // Show list of options for registered users.
+        // 1. Option to sign out
+        // 2. Option to edit profile details
+        // 3. Option to view stats / content
+    )
+}
