78357eb17c6e1fc068240c6ff8b77479f7f37dee
GUL-Bao <BaoPham92@yahoo.com>
2019-09-29 21:23:43 -0400

    TODO Priorities

---
+++ client/src/components/view/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// * HOME PAGE FOR VUEBLOC V0
+
+// TODO: LIVE DISPLAY USER ACTIVITY STATISITCS
+// TODO:
+// TODO:
+// TODO:
+// TODO:
+
+export const HomePage = () => {
+
+    return (
+        <div>
+            <h1>Home Page Component</h1>
+        </div>
+    )
+}
