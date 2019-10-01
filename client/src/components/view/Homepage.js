bff791d00a90071ed0fa78c1364ec611e7dfc125
GUL-Bao <BaoPham92@yahoo.com>
2019-10-01 12:59:17 -0400

    Updated TODOS

--- client/src/components/view/Homepage.js
+++ client/src/components/view/Homepage.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 // * HOME PAGE FOR VUEBLOC V0
 
-// TODO: LIVE DISPLAY USER ACTIVITY STATISITCS
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-
 export const HomePage = () => {
 
     return (
         <div>
+            // TODO: DISPLAY SIDE SECTION THAT WOULD ALLOW TRAVERSING OF SITE (IE: SUB-FORUMS, ACTIVITIES, ETC) - LEFT OF PAGE
+
+            // TODO: USER INFO / NAV BAR (IE: DISPLAYING USER STATISTICS, PM'S, NOTIFICATIONS) - DISPLAY ONLY IF LOGGED IN - OTHERWISE BASIC NAVBAR FOR SIGN IN - SIGN UP
             <h1>Home Page Component</h1>
+
+            // TODO: DISPLAY COMPONENTS OF LIVE STATS & DATA OF RECENTLY VISISTED SUBS, NEW POSTS, ACTIVITY, ETC. - MIDDLE OF PAGE
+
+            // TODO: LIVE DISPLAY MISC ACTIVITY STATISITCS (IE:Trends component - display tredning posts) - RIGHT OF PAGE
         </div>
     )
 }
